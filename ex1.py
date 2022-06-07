32d8deb3377a0e2044e697583f4147b67782c60a
YanaAfanaseva <2947468@gmail.com>
2022-06-07 15:31:11 +0200

    first_solution_of_Ex1

--- ex1.py
+++ ex1.py
@@ -1,19 +1,21 @@
 A = [1,1,2,3,3]
 B = [2,4,4,6]
-K = 3
+C = [1,1,3]
+
 
 
 def solution(A, K):
     n = len(A)
     for i in range(n - 1):
-        #compare current number
-        if (A[i] + 1 < A[i + 1]):
+        #check if this list is a non-decreasing order
+        if (A[i] > A[i + 1]):
             return False
         if (A[0] != 1 and A[n - 1] != K):
             return False
         else:
             return True
 
-# print(solution(A,K))
-# print(solution(B,8))
+print(solution(A,3))
+print(solution(C,2))
 print(solution(B,6))
+print(solution(B,8))
